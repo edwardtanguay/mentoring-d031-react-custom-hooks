cd26c55e7aebc4513d474d5b79638afb890b541a
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 11:36:18 +0200

    userIsOnline works locally

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -1,14 +1,26 @@
-export const PageUseToggle = () => {
+import { useState } from 'react';
 
-	const userIsOnline = false;
+export const PageUseToggle = () => {
+	const [userIsOnline, setUserIsOnline] = useState(false);
+	const toggleUserIsOnline = () => {
+		const _userIsOnline = userIsOnline ? false : true;
+		setUserIsOnline(_userIsOnline);
+	}
 	return (
 		<div className="page_useToggle">
 			<h2>UseToggle</h2>
-			{userIsOnline ? (
-				<p>User is currently online</p>
-			) : (
-				<p>User is OFFLINE.</p>
-			)}
+			<div className="userIsOnlineArea">
+				<div className="message">
+					{userIsOnline ? (
+						<p>User is currently online</p>
+					) : (
+						<p>User is OFFLINE.</p>
+					)}
+				</div>
+				<div className="buttonArea">
+					<button onClick={toggleUserIsOnline}>Toggle</button>
+				</div>
+			</div>
 		</div>
 	);
 };
