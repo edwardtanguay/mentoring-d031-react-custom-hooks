6ec92d17583a78f75c3d65b4d1ecae77bdc6f44d
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 11:30:26 +0200

    const test

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -1,8 +1,14 @@
 export const PageUseToggle = () => {
+
+	const userIsOnline = false;
 	return (
 		<div className="page_useToggle">
 			<h2>UseToggle</h2>
-			<p>nnn</p>
+			{userIsOnline ? (
+				<p>User is currently online</p>
+			) : (
+				<p>User is OFFLINE.</p>
+			)}
 		</div>
-	)
-}
+	);
+};
