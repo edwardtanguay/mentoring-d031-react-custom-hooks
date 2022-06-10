b09c3bdb407f29fc256bba84c9d63bc26c5f8422
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 11:48:35 +0200

    project is completed works

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -2,10 +2,12 @@ import { useToggle } from '../hooks/useToggle';
 
 export const PageUseToggle = () => {
 	const [userIsOnline, toggleUserIsOnline] = useToggle(false);
+	const [projectIsCompleted, toggleProjectIsCompleted] = useToggle(true);
 
 	return (
 		<div className="page_useToggle">
 			<h2>UseToggle</h2>
+
 			<div className="userIsOnlineArea">
 				<div className="message">
 					{userIsOnline ? (
@@ -18,6 +20,20 @@ export const PageUseToggle = () => {
 					<button onClick={toggleUserIsOnline}>Toggle</button>
 				</div>
 			</div>
+
+			<div className="projectIsCompletedArea">
+				<div className="message">
+					{projectIsCompleted ? (
+						<p>Project is completed.</p>
+					) : (
+						<p>Project is NOT COMPLETED.</p>
+					)}
+				</div>
+				<div className="buttonArea">
+					<button onClick={toggleProjectIsCompleted}>Toggle</button>
+				</div>
+			</div>
+
 		</div>
 	);
 };
