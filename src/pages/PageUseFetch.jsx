a0a7d1764ec9858029e381c801f8d02c98156525
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 11:59:53 +0200

    tech books work

--- src/pages/PageUseFetch.jsx
+++ src/pages/PageUseFetch.jsx
@@ -1,11 +1,31 @@
-const verbsUrl = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
+import { useFetch } from '../hooks/useFetch';
+
 const techBooksUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
+const verbsUrl = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
 
 export const PageUseFetch = () => {
+	const {
+		items: techBooks,
+		totalItems: totalTechBooks,
+		isLoading: techBooksAreLoading,
+	} = useFetch(techBooksUrl, true);
+
 	return (
 		<div className="page_useFetch">
 			<h2>useFetch</h2>
-			<p>nnn</p>
+			<p>Tech Books</p>
+			{techBooksAreLoading ? (
+				<h3>Tech books are loading...</h3>
+			) : (
+				<>
+					<h3>There are {totalTechBooks} tech books.</h3>
+					<ul>
+						{techBooks.map((techBook, index) => {
+							return <li key={index}>{techBook.title}</li>;
+						})}
+					</ul>
+				</>
+			)}
 		</div>
 	);
 };
