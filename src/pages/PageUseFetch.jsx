b5c74da579eb6227f0014c72093d56c11b244005
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 11:52:06 +0200

    page useFetch created

---
+++ src/pages/PageUseFetch.jsx
@@ -0,0 +1,10 @@
+const verbsUrl = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
+
+export const PageUseFetch = () => {
+	return (
+		<div className="page_useFetch">
+			<h2>useFetch</h2>
+			<p>nnn</p>
+		</div>
+	);
+};
